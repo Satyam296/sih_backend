1277a2c533bd30979d554264307ed8a60140d107
Satyam Chhetri <satyamchhetri629@gmail.com>
2025-10-16 09:52:14 +0530

    Fix CORS configuration for localhost origins

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "http://localhost:3001",
+      "http://localhost:3001"],
     methods: ["GET", "POST"],
   },
 });
